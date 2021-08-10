c06a0e95aec07db051d92eb9a4756d3a7016dbde
Dmitrusty <dmitry__@ukr.net>
2021-08-10 19:00:54 +0300

    Начал внедрять Spring Data JPA

---
+++ src/main/java/com/example/cities/config/CitiesRepository.java
@@ -0,0 +1,8 @@
+package com.example.cities.config;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.Repository;
+
+public interface CitiesRepository extends Repository {
+
+}
